0b794e44bae7599bd233045af97dd5fa5131407b
UmaMahesh28 <uxb29200@ucmo.edu>
2023-04-18 02:38:41 +0530

    codechanges

--- uploadWallpapers.py
+++ uploadWallpapers.py
@@ -1,19 +1,21 @@
 import boto3
 import os
+import mysql.connector
+import datetime
+import time
+
+current_time = datetime.datetime.now()
 
 # Set up AWS credentials
 s3 = boto3.resource('s3')
 
 import boto3
 
-
-import boto3
-import json
 # create S3 bucket instance
-#s3 = boto3.client('s3')
+#s3client = boto3.client('s3')
 bucket_name = 'mybucketforwallpaper'
 '''
-s3.create_bucket(Bucket=bucket_name)
+s3client.create_bucket(Bucket=bucket_name)
 # define the policy
 bucket_policy = {
      'Version': '2012-10-17',
@@ -33,9 +35,55 @@ bucket_policy = {
 #bucket_name = 'mydemobucket0'
 folder_path = "C:\\Users\\umama\\OneDrive\\Desktop\\AWSProject\\Wallpapers"
 
+def upload_images(folder_path):
 # Iterate through all files in the folder and upload each one to S3
-for file_name in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, file_name)
-    key_name = file_name
-    s3.Bucket(bucket_name).upload_file(file_path, key_name)
-    print(f'{file_name} uploaded to S3 bucket {bucket_name} as {key_name}')
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        key_name = file_name
+        s3.Bucket(bucket_name).upload_file(file_path, key_name)
+        print(f'{file_name} uploaded to S3 bucket {bucket_name} as {key_name}')
+
+
+def insert_data():
+    s3 = boto3.resource('s3')
+    bucket_name = 'mybucketforwallpaper'
+
+    bucket = s3.Bucket(bucket_name)
+    mydb = mysql.connector.connect(
+    host = "my-rds-instance.cb3awmiitwm0.us-east-1.rds.amazonaws.com",
+    user = "admin",
+    password = "mypassword",
+    database = "mydatabase"
+    )
+    for obj in bucket.objects.all():
+                # Generate a presigned URL for the object
+        url = s3.meta.client.generate_presigned_url(
+                    'get_object',
+                    Params={
+                        'Bucket': bucket_name,
+                        'Key': obj.key
+                    }
+                )
+
+                #wallpapers.append(url)
+                #filenames.append(filenames)
+                # Print the URL
+        print(url)
+
+                # Query the RDS database for the download counts of each wallpaper
+        mycursor = mydb.cursor()
+        mycursor1 = mydb.cursor()
+
+        sql = "INSERT INTO wallpapers (FileName, bucket_name,path,create_time) VALUES (%s,%s,%s,%s)"
+        val = (obj.key, bucket_name,url,current_time)
+
+        mycursor.execute(sql, val)
+        mydb.commit()
+    print(mycursor.rowcount)
+
+upload_images(folder_path)
+
+#adding delay to the code
+time.sleep(10)
+
+insert_data()
--- wallpapersite.py
+++ wallpapersite.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, send_file
 import boto3
-import os
-import pymysql
+import json
+import mysql.connector
+import datetime;
+ 
+# ct stores current time
+ct = datetime.datetime.now()
 
 app = Flask(__name__)
 
@@ -10,54 +14,46 @@ BUCKET_NAME = 'mybucketforwallpaper'
 REGION_NAME = 'us-east-1'
 
 # AWS RDS database configuration
-DB_HOST = 'my-rds-instance.cb3awmiitwm0.us-east-1.rds.amazonaws.com'
-DB_PORT = 3306
-DB_NAME = 'mydatabase'
-DB_USER = 'admin'
-DB_PASSWORD = 'mypassword'
-
 # Connect to the RDS database
-#conn = pymysql.connect(host=DB_HOST, port=DB_PORT, db=DB_NAME, user=DB_USER, password=DB_PASSWORD)
+mydb = mysql.connector.connect(
+  host = "my-rds-instance.cb3awmiitwm0.us-east-1.rds.amazonaws.com",
+  user = "admin",
+  password = "mypassword",
+  database = "mydatabase"
+)
+
+
 
 @app.route('/')
 def index():
+    try:
     # Get a list of all wallpapers from S3 bucket
-    '''s3 = boto3.client('s3', region_name=REGION_NAME)
-    response = s3.list_objects_v2(Bucket=BUCKET_NAME)
-    print(response)
-    wallpapers = [obj['Key'] for obj in response['Contents']]
-    print(wallpapers)'''
-    import boto3
+        
+        '''import boto3
 
-    s3 = boto3.resource('s3')
-    bucket_name = 'mybucketforwallpaper'
+        s3 = boto3.resource('s3')
+        bucket_name = 'mybucketforwallpaper'
 
-    bucket = s3.Bucket(bucket_name)
+        bucket = s3.Bucket(bucket_name)
+        s3client = boto3.client('s3')'''
+        wallpapers= []
+        # Iterate through all objects in the bucket
+        
+        query = "SELECT path FROM wallpapers"
+        mycursor1 = mydb.cursor()
+        mycursor1.execute(query)
+        myresult = mycursor1.fetchall()
+        print(myresult)
+        for x in myresult:
+           wallpapers.append(x[0])
+        print(wallpapers)
+        
+        #print(fileList)
+        return render_template('index.html', wallpapers=wallpapers)#, download_counts=download_counts)
+    except:
+        print("error")
 
-    wallpapers= []
-    # Iterate through all objects in the bucket
-    for obj in bucket.objects.all():
-        # Generate a presigned URL for the object
-        url = s3.meta.client.generate_presigned_url(
-            'get_object',
-            Params={
-                'Bucket': bucket_name,
-                'Key': obj.key
-            }
-        )
-        wallpapers.append(url)
-
-        # Print the URL
-        print(url)
-
-        # Query the RDS database for the download counts of each wallpaper
-        #cursor = conn.cursor()
-        #cursor.execute('SELECT filename, downloads FROM wallpapers')
-        #download_counts = {row[0]: row[1] for row in cursor.fetchall()}
-    
-    return render_template('index.html', wallpapers=wallpapers)#, download_counts=download_counts)
-
-@app.route('/wallpapers/<filename>')
+'''@app.route('/wallpapers/<filename>')
 def wallpaper(filename):
     # Increment the download count of the requested wallpaper in the RDS database
     #cursor = conn.cursor()
@@ -67,7 +63,7 @@ def wallpaper(filename):
     # Download the requested wallpaper from S3 bucket and return it as a file
     s3 = boto3.client('s3', region_name=REGION_NAME)
     response = s3.get_object(Bucket=BUCKET_NAME, Key=filename)
-    return send_file(response['Body'], attachment_filename=filename)
+    return send_file(response['Body'], attachment_filename=filename)'''
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
